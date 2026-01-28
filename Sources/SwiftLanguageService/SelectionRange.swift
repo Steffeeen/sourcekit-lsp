049db5b82025a27a02539f2e0b648cba29baa4c4
Steffen <steffaeaeaen@gmail.com>
2026-01-28 12:49:39 +0100

    Don't create selection ranges for key path component (list)
    This fixes two tests:
    - testKeyPathExpression
    - testKeyPathSubscript

--- Sources/SwiftLanguageService/SelectionRange.swift
+++ Sources/SwiftLanguageService/SelectionRange.swift
@@ -161,7 +161,7 @@ private func calculateRangesFor(
   case .patternBindingList, .initializerClause, .matchingPatternCondition, .exprList,
     .accessorDeclList, .functionParameterClause, .functionEffectSpecifiers, .switchCaseLabel, .switchCaseList,
     .inheritanceClause, .inheritedType, .memberBlockItemList, .memberBlock, .enumCaseParameterClause,
-    .optionalChainingExpr, .tuplePatternElement, .arrayElement:
+    .optionalChainingExpr, .tuplePatternElement, .arrayElement, .keyPathComponent, .keyPathComponentList:
     return []
 
   default:
